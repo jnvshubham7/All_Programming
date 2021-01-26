61f2c374ba11cc67356fa323cb78c97f0b1840f9
Shubham Kumar Bhokta <69629803+jnvshubham7@users.noreply.github.com>
2021-01-26 18:27:24 +0530

    1

---
+++ 52.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int firstnumber, secondnumber, sumoftwonumber;
+
+    cout << "enter two integer: ";
+    cin >> firstnumber >> secondnumber;
+
+    sumoftwonumber = firstnumber + secondnumber;
+
+    cout<< firstnumber << "+" << secondnumber << "=" << sumoftwonumber;
+
+    return 0;
+}
