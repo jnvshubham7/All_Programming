fa3444eccfb42de517c0a10b65b4b129b5e84757
Shubham Kumar Bhokta <69629803+jnvshubham7@users.noreply.github.com>
2021-01-28 08:11:44 +0530

    1

---
+++ 3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1 = 2;
+    double num2 = 2.2;
+    char ch = 'A';
+    cout << num1 << endl;
+    cout << num2 << endl;
+    cout << "character : " << ch << endl;
+    return 0;
+
+
+}
---
+++ 4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout << "enter a integer number: ";
+    cin >> num;
+    cout << "integer number is: " << num;
+    return 0;  
+}
