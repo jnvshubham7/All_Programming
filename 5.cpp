484f3543b1d3588c4ac920fb569201244c9df050
Shubham Kumar Bhokta <69629803+jnvshubham7@users.noreply.github.com>
2021-02-04 17:26:13 +0530

    Shubham

---
+++ 5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+
+{
+ int num;
+ char a;
+ cout << "enter a integer and chacater: ";
+ cin  >> a >> num;
+
+ cout << "Character: " << a << endl;
+ cout << "Number: " << num;
+ return 0;
+ 
+    
+}
