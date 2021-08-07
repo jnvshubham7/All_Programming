6b289d7aceb9f1c4499ec049c4d05b3d6c6cb74d
Shubham Kumar Bhokta <69629803+jnvshubham7@users.noreply.github.com>
2021-08-08 05:08:55 +0530

    Linux New File Add

---
+++ 96.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+	
+// main function
+// where the execution
+// of program begins
+int main()
+{
+	// prints Hello World!
+	cout<<"Hello World!\n";
+		
+	return 0;
+}
+
---
+++ hello.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+            cout<<“Linux”<<endl;
+
+            return 0;
+
+}
