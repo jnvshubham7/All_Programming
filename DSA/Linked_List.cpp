14c067e86b8fb271e8c144dea143ad86f4d26e7d
Shubham Kumar Bhokta <69629803+jnvshubham7@users.noreply.github.com>
2021-12-24 10:44:37 +0530

    update

--- DSA/Linked_List.cpp
+++ DSA/Linked_List.cpp
@@ -1,48 +1,41 @@
 // Linked list implementation in C++
-#include<bits/stdc++.h>
+
+#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-#define fileio freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout)
 
+// Creating a node
 class Node {
-    public:
-       int value;
-         Node *next;
-
-       
+   public:
+  int value;
+  Node* next;
 };
 
-int main(){
-   // fileio;
+int main() {
+  Node* head;
+  Node* one = NULL;
+  Node* two = NULL;
+  Node* three = NULL;
 
-Node* head;
-Node* one = NULL;
-Node* two = NULL;
-Node* three = NULL;
+  // allocate 3 nodes in the heap
+  one = new Node();
+  two = new Node();
+  three = new Node();
 
+  // Assign value values
+  one->value = 1;
+  two->value = 2;
+  three->value = 3;
 
-one = new Node();
-two = new Node();
-three = new Node();
+  // Connect nodes
+  one->next = two;
+  two->next = three;
+  three->next = NULL;
 
-one->value = 1;
-two->value = 2;
-three->value = 3;
-
-one->next = two;
-two->next = three;
-three->next = NULL;
-
-
-head = one;
-
-while(head != NULL){
-
-
-    cout<<head->value<<" ";
+  // print the linked list value
+  head = one;
+  while (head != NULL) {
+    cout << head->value;
     head = head->next;
-}
-
-    
-
-   
+  }
 }
