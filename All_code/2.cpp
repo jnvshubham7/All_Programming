d113f51ba10ce21866a4e9e5f9ffb393b0c4876e
Shubham Kumar Bhokta <69629803+jnvshubham7@users.noreply.github.com>
2022-03-04 00:10:10 +0530

    update

--- All_code/2.cpp
+++ All_code/2.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 int main()
 {
-     freopen("input.txt", "r", stdin);
-freopen("output.txt", "w", stdout);
+//      freopen("input.txt", "r", stdin);
+// freopen("output.txt", "w", stdout);
 
     std::cout<< "Shubham Kumar Bhokta" ;
     return 0;
--- Z--Testing.cpp
+++ Z--Testing.cpp
@@ -1,31 +1,58 @@
-#include <bits/stdc++.h> using namespace std; int solve(string&s,string& t,int n,int m)
-{
-  if (n < 0 || m < 0)
-    return 0;
-  else if (n == 0 && m == 0)
-    return 1;
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
 
-  else if (m == 0)
-    return 1;
-  else if (dp[n][m] != -1)
-    return dp[n][m];
-  else {
-    int ans = 0;
-    if (s[n] == t[m]) {
-      ans = solve(s, t, n - 1, m - 1) + solve(s, t, n - 1, m);
-    } else {
-      ans += solve(s, t, n - 1, m);
-    }
-    dp[n][m] = ans;
-    return ans;
+int main(){
+
+int t;
+cin>>t;
+while(t--){
+
+  int n;
+  cin>>n;
+  int a[n];
+  for(int i=0;i<n;i++)
+  {
+    cin>>a[i];
   }
+
+   //find the maximum element in the array
+  int max=a[0];
+  for(int i=1;i<n;i++)
+  {
+    if(a[i]>max)
+    {
+      max=a[i];
+    }
+  }
+  //find the minimum element in the array
+
+  int min=a[0];
+  for(int i=1;i<n;i++)
+  {
+    if(a[i]<min)
+    {
+      min=a[i];
+    }
+  }
+  //find the maximum difference between the maximum and minimum element
+  int diff=max-min;
+  for(int i=0;i<n;i++)
+  {
+    if(a[i]>min && a[i]<max)
+    {
+      if(a[i]-min>diff)
+      {
+        diff=a[i]-min;
+      }
+    }
+  }
+  cout<<diff<<endl;
+  
+  
+
+
 }
-int main() {
-  string s, t;
-  cin >> s >> t;
-  int n = s.size();
-  int m = t.size();
-  int i, j;
-  int ans = solve(s, t, n - 1, m - 1);
-  cout << ans << endl;
+
+  return 0;
 }
