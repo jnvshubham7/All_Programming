f1ae5d094c2149b1c053e0e584fd19f189781270
Shubham Kumar Bhokta <69629803+jnvshubham7@users.noreply.github.com>
2022-03-05 15:15:49 +0530

    update

--- LeetCode/0_Testing.cpp
+++ LeetCode/0_Testing.cpp
@@ -1,46 +1,8 @@
-
-    
-    vector<int> canMakeTriangle(vector<int> A, int N){
-        // code here
-
-         vector<int> v((n-2),0);
-        
-        for(int i=0; i<=n-3; i++)
-        {
-           int a=arr[i];
-           int b=arr[i+1];
-           int c=arr[i+2];
-           
-           if(((a+b)>c && (a+c)>b) && (b+c)>a)
-           {
-               v[i]=1;
-           }
-           else
-           {
-               v[i]=0;
-           }
-        }
-        
-        return v;
-
-        //take a[i] , a[i+1] , a[i+2] and check if they can form a triangle
-        // if yes, return 1 else return 0
-
-        // vector<int> ans;
-        // for(int i=0;i<N-2;i++){
-        //     for(int j=i+1;j<N-1;j++){
-        //         for(int k=j+1;k<N;k++){
-        //             if(A[i]+A[j]>A[k]){
-        //                 ans.push_back(1);
-        //                 break;
-        //             }
-        //             else{
-        //                 ans.push_back(0);
-        //             }
-        //         }
-        //     }
-        // }
-        // return ans;
+ long long findMaxScore(Node* root)
+    {
+        // Your code goes here
+        if(root== NULL) return 1;
+        if(root->left==NULL) return (root->data)*(findMaxScore(root->right));
+        else if(root->right==NULL) return (root->data)*(findMaxScore(root->left));
+        else return max((root->data)*(findMaxScore(root->right)),(root->data)*(findMaxScore(root->left)));   
     }
-
-   
--- LeetCode/413.cpp
+++ LeetCode/413.cpp
@@ -2,6 +2,39 @@ class Solution {
 public:
     int numberOfArithmeticSlices(vector<int>& nums) {
 
+ int curr = 0, total = 0;
+        for (int i=2; i<nums.size(); i++){
+            if (nums[i]-nums[i-1] == nums[i-1]-nums[i-2]) {
+                curr += 1;
+                total += curr;
+            } else {
+                curr = 0;
+            }
+        }
+        return total;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //         An integer array is called arithmetic if it consists of at least three elements and if the difference between any two consecutive elements is the same.
 
 // For example, [1,3,5,7,9], [7,7,7,7], and [3,-1,-5,-9] are arithmetic sequences.
@@ -9,7 +42,13 @@ public:
 
 // A subarray is a contiguous subsequence of the array.
 
+// int n=nums.size();
+// if(n<3) return 0;
 
-        
+
     }
+
+
+
+
 };
