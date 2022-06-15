0ea0141ebbf4ed2ede8f8e68e155cdb89555d328
Shubham Kumar Bhokta <69629803+jnvshubham7@users.noreply.github.com>
2022-06-15 21:13:05 +0530

    1

---
+++ LeetCode/92.reverse-linked-list-ii.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=92 lang=cpp
+ *
+ * [92] Reverse Linked List II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        
+    }
+};
+// @lc code=end
+
