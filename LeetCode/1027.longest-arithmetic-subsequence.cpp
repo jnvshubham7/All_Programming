60b64eef51e21f3109a2d75865a30d1c0b0b32e1
Shubham Kumar Bhokta <69629803+jnvshubham7@users.noreply.github.com>
2022-06-15 22:18:23 +0530

    1

---
+++ LeetCode/1027.longest-arithmetic-subsequence.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1027 lang=cpp
+ *
+ * [1027] Longest Arithmetic Subsequence
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int longestArithSeqLength(vector<int>& nums) {
+        
+    }
+};
+// @lc code=end
+
--- LeetCode/92.reverse-linked-list-ii.cpp
+++ LeetCode/92.reverse-linked-list-ii.cpp
@@ -18,6 +18,8 @@
 class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
+
+        
         
     }
 };
