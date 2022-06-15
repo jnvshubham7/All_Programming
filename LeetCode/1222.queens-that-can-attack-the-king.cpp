77f15e889f2b7c75d24e96ccfe1a4f52a2d2cbd4
Shubham Kumar Bhokta <69629803+jnvshubham7@users.noreply.github.com>
2022-06-15 21:08:28 +0530

    1

---
+++ LeetCode/1222.queens-that-can-attack-the-king.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1222 lang=cpp
+ *
+ * [1222] Queens That Can Attack the King
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> queensAttacktheKing(vector<vector<int>>& queens, vector<int>& king) {
+        
+    }
+};
+// @lc code=end
+
---
+++ LeetCode/153.find-minimum-in-rotated-sorted-array.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=153 lang=cpp
+ *
+ * [153] Find Minimum in Rotated Sorted Array
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        
+    }
+};
+// @lc code=end
+
