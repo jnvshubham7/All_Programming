ede2891d4af723209263e469df2d2b41d7e56c51
Shubham Kumar Bhokta <69629803+jnvshubham7@users.noreply.github.com>
2022-06-19 16:09:21 +0530

    1

--- LeetCode/745.prefix-and-suffix-search.cpp
+++ LeetCode/745.prefix-and-suffix-search.cpp
@@ -10,38 +10,75 @@ public:
   unordered_map<string, int> mp;
 
     WordFilter(vector<string>& words) {
-        int n = words.size();
+
+        int n=words.size();
         for(int i=0;i<n;i++)
         {
-            mp[words[i]]=i;
+            string str=words[i];
+            int m=str.size();
+
+            for(int j=1;j<=m;j++)
+            {
+                string p=str.substr(0,j);
+
+                for(int k=0;k<m;k++)
+                {
+                    string s=str.substr(k,m);
+
+                    mp[p+ "|" +s]=i+1;
+                }
+            }
         }
+
+       
+       }
+
+
+
+        // int n = words.size();
+        // for(int i=0;i<n;i++)
+        // {
+        //     mp[words[i]]=i;
+        // }
 
 
 
         
-    }
+    
     
     int f(string prefix, string suffix) {
 
-        string temp;
-        int res=-1;
-        for(auto x:mp){
-            if(x.first.size()>=prefix.size() && x.first.size()>=suffix.size()){
-                temp=x.first.substr(0,prefix.size());
-                if(temp==prefix){
-                    temp=x.first.substr(x.first.size()-suffix.size(),suffix.size());
-                    if(temp==suffix){
-                       // return x.second;
-                        return res=max(res,x.second);
-                    }
-                }
+        string s=prefix+"|" +suffix;
 
-        }
-        }
-        return res;
+       return mp[s]-1;
 
 
-       
+        
+        
+
+      
+
+
+
+        // string temp;
+        // int res=-1;
+        // for(auto x:mp){
+        //     if(x.first.size()>=prefix.size() && x.first.size()>=suffix.size()){
+        //         temp=x.first.substr(0,prefix.size());
+        //         if(temp==prefix){
+        //             temp=x.first.substr(x.first.size()-suffix.size(),suffix.size());
+        //             if(temp==suffix){
+        //                // return x.second;
+        //                 return res=max(res,x.second);
+        //             }
+        //         }
+
+        // }
+        // }
+        // return res;
+
+
+    
         
     }
 
@@ -53,4 +90,65 @@ public:
  * int param_1 = obj->f(prefix,suffix);
  */
 // @lc code=end
+
+
+
+
+
+
+
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main(){
+
+
+// Input
+// ["WordFilter", "f"]
+// [[["apple"]], ["a", "e"]]
+
+vector<string> words = {"apple"};
+
+unordered_map<string, int> mp;
+
+
+     int n = words.size();
+        for (int i = 0; i < n; i++) {
+            string word = words[i];
+            int wordSize = word.size();
+            for (int j = 1; j <= wordSize; j++) {
+                string p = word.substr(0, 1);
+                for (int k = 0; k < wordSize; k++) {
+                    string s = word.substr(wordSize-1, wordSize);
+                    mp[p + "|" + s] = i + 1;
+
+                   
+                       
+                }
+            }
+        }
+
+        //print mp
+
+        for(auto x:mp){
+            cout<<x.first<<" "<<x.second<<endl;
+        }
+
+
+
+         
+
+       
+        
+
+
+
+    return 0;
+}
+
 
---
+++ LeetCode/987.vertical-order-traversal-of-a-binary-tree.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=987 lang=cpp
+ *
+ * [987] Vertical Order Traversal of a Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        
+    }
+};
+// @lc code=end
+
