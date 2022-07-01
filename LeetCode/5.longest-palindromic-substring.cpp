4814fb9aceb714e3310cacee2f36830103f266e9
Shubham Kumar Bhokta <69629803+jnvshubham7@users.noreply.github.com>
2022-07-01 19:08:16 +0530

    1

--- LeetCode/5.longest-palindromic-substring.cpp
+++ LeetCode/5.longest-palindromic-substring.cpp
@@ -4,6 +4,26 @@
  * [5] Longest Palindromic Substring
  */
 
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define vi vector<int>
+#define pii pair<int,int>
+#define fr(i,n) for(int i=0;i<n;i++)
+#define all(v) v.begin(),v.end()
+#define mem(a,b) memset(a,b,sizeof(a))
+#define MOD 1000000007
+#define pb push_back
+#define mp make_pair
+#define pii pair<int,int>
+#define vll vector<ll>
+#define vc vector<char>
+#define vs vector<string>
+    
+
+
+
+
 // @lc code=start
 class Solution {
 public:
@@ -78,3 +98,21 @@ string solve(string s1,string s2)
 // @lc code=end
 
 
+
+
+
+
+int main(){
+Solution s;
+
+ll t;
+cin>>t;
+while(t--){
+
+    
+
+
+}
+
+    return 0;
+}
