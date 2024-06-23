1e79ab36e302b448106a8f4e34512eea014038df
Shubham Kumar Bhokta <69629803+jnvshubham7@users.noreply.github.com>
2024-06-23 23:20:35 +0530

    1

--- 1.cpp
+++ 1.cpp
@@ -3,35 +3,42 @@ using namespace std;
 
 int main(){
 
+
+    cout<<"number :"<<" "<<endl;
+    
+
     int n;
     cin>>n;
-    vector<int> a(n);
 
+    vector<int> arr(n);
     for(int i=0;i<n;i++){
-        cin>>a[i];
-    }
-
-    int len =INT_MIN;
-
-    int cnt=1;
-
-
-    for(int i=1;i<n;i++)
-    {
-        if(arr[i]>arr[i-1])
-        {
-            cnt++;
-        }
-        else
-        {
-            len=max(len,cnt);
-            cnt=1;
-        }
-
+        cin>>arr[i];
 
     }
 
-    cout<<len<<endl;
+    
+
+    // sort(arr.begin(),arr.end());
+
+
+    // cout<<arr[0]; 
+
+    // int temp=INT_MAX;
+
+    // for(int i=0;i<n;i++)
+    // {
+    //     if(arr[i]<temp)
+    //     {
+    //         temp=arr[i];
+
+    //     }
+    // }
+
+
+    // cout<<temp<<endl;
+
+
+
 
 
     return 0;
